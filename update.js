b760da46fef52b474e358ecb34c057bdb1ff6abf
Vasilis Kefallinos <vkefallinos@gmail.com>
2020-04-17 23:27:12 +0300

    add test from owid

--- update.js
+++ update.js
@@ -5,11 +5,42 @@ const parse = require("csv-parse/lib/sync");
 const FILENAME_CONFIRMED = "time_series_covid19_confirmed_global.csv";
 const FILENAME_DEATHS = "time_series_covid19_deaths_global.csv";
 const FILENAME_RECOVERED = "time_series_covid19_recovered_global.csv";
+const FILENAME_TESTS = "covid-testing-all-observations.csv";
+
+function extractTests(filepath, countries) {
+  const csv = fs.readFileSync(filepath);
+  const data = parse(csv);
+  // console.log(data)
+  const timeseriesPerEntity = data.reduce((perCountry, entry)=>{
+    const [ientity, date, source, sourceLabel,notes, testsTotal] = entry
+    const entity = ientity.split("-")[0]
+    if(!perCountry[entity]){
+      perCountry[entity] = {}
+    }
+    const normalDate = new Date(date)
+    perCountry[entity][`${normalDate.getFullYear()}-${normalDate.getMonth()+1}-${normalDate.getDate()}`] = parseInt(testsTotal)
+    return perCountry
+  }, {})
+  const timeseriesPerCountry = {}
+  timeseriesPerEntity["Czechia"] = timeseriesPerEntity["Czech Republic "]
+  delete timeseriesPerEntity["Czech Republic "]
+  for(const entity in timeseriesPerEntity){
+    const countryName = countries.find((country)=>{
+      return entity.match(country)
+    })
+    if(!countryName){
+      console.error( entity)
+    }
+    timeseriesPerCountry[countryName] = timeseriesPerEntity[entity]
+  }
+  return [timeseriesPerCountry]
+
+}
 
 function extract(filepath) {
   const csv = fs.readFileSync(filepath);
   const [headers, ...rows] = parse(csv);
-  const [province, country, lat, long, ...dates] = headers;
+  let [province, country, lat, long, ...dates] = headers;
   const countList = {};
 
   // HACK: CSVs have different date formats
@@ -19,12 +50,17 @@ function extract(filepath) {
   });
 
   rows.forEach(([province, country, lat, long, ...counts]) => {
+    if(country.includes(",")){
+      const [part1, part2] = country.split(",")
+      country = `${part2.trim()} ${part1.trim()}`
+    }
     countList[country] = countList[country] || {};
     normalDates.forEach((date, i) => {
       countList[country][date] = countList[country][date] || 0;
       countList[country][date] += +counts[i];
     });
   });
+  // console.log(countList)
   return [countList, normalDates];
 }
 
@@ -38,8 +74,9 @@ function update(dataPath, outputPath) {
   const [deaths] = extract(path.resolve(dataPath, FILENAME_DEATHS));
   const [recovered] = extract(path.resolve(dataPath, FILENAME_RECOVERED));
   const countries = Object.keys(confirmed);
+  const [tests] = extractTests(path.resolve(dataPath, FILENAME_TESTS), countries);
   const results = {};
-
+  console.log(tests["Greece"])
   countries.forEach(country => {
     // Some country names are different in the recovered dataset
     const recoverdCountry = patchCountryNames[country] || country;
@@ -47,12 +84,15 @@ function update(dataPath, outputPath) {
     if (!recovered[recoverdCountry]) {
       console.warn(`${recoverdCountry} is missing from the recovered dataset`);
     }
-
+    let lastTests = 0
     results[country] = dates.map(date => {
+      const totalTests = tests[country] && tests[country][date] || lastTests
+      lastTests = totalTests
       return {
         date,
         confirmed: confirmed[country][date],
         deaths: deaths[country][date],
+        tests: totalTests,
         recovered:
           recovered[recoverdCountry] && recovered[recoverdCountry][date] != null
             ? recovered[recoverdCountry][date]
